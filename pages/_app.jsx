37336b337e36ca5a7c3b02530404f7454f1bcee5
Muhammad Aiman Arif Bin Baharudin <77523218+Aiman217@users.noreply.github.com>
2023-04-15 22:00:06 +0800

    Update name again.

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -2,7 +2,7 @@ import "../styles/globals.css";
 import { createBrowserSupabaseClient } from "@supabase/auth-helpers-nextjs";
 import { SessionContextProvider } from "@supabase/auth-helpers-react";
 import { useState } from "react";
-import Navbar from "@/components/Header/Nav";
+import Navbar from "@/components/header/Nav";
 
 export default function MyApp({ Component, pageProps }) {
   const [supabase] = useState(() => createBrowserSupabaseClient());
