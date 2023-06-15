7d3f5e2f1195343ddeca9864fe93ffbaf5194bd7
Muhammad Aiman Arif Bin Baharudin <aiman_official_217@proton.me>
2023-06-15 14:09:14 +0800

    Update package.

--- pages/dashboard/profile/index.jsx
+++ pages/dashboard/profile/index.jsx
@@ -14,6 +14,10 @@ export default function index() {
             <p className="text-md mb-4">Update your profile.</p>
           </div>
         </div>
+        <input
+          type="file"
+          className="file-input file-input-bordered file-input-primary w-full max-w-xs"
+        />
       </div>
     </>
   );
